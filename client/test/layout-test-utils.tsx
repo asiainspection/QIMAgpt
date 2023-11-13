45250d061084eb7fd62fb2f895c84449bbfab1ad
Jamie.Wu <jamie.wu@qima.com>
2023-11-13 10:08:52 +0800

    merge(QIMAGpt): SP-14641 Sync the LibreChat repository code to QIMAGPT

--- client/test/layout-test-utils.tsx
+++ client/test/layout-test-utils.tsx
@@ -1,3 +1,4 @@
+import './matchMedia.mock';
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
