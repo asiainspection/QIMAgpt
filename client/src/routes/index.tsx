b26a1071dbec178b6b7b508fb29445b8ef38d993
Jamie Wu <jamie.wu@qima.com>
2024-05-20 10:02:30 +0800

    eat(update): SP-17091 update qimagpt to v0.7.30-rc

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -56,6 +56,10 @@ export const router = createBrowserRouter([
           },
         ],
       },
+      {
+        path: '*',
+        element: <Navigate to="/c/new" replace={true} />,
+      },
     ],
   },
 ]);
