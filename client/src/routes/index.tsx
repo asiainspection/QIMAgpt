0de9c2d78da52cb8646db9116eb7b2d89b7ae9c3
Jamie Wu <jamie.wu@qima.com>
2024-07-15 16:48:13 +0800

    feat(QIMAGPT): SP-17967 update QIMAGpt version to v0.7.4-rc1

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -53,8 +53,6 @@ export const router = createBrowserRouter([
     element: <AuthLayout />,
     children: [
       {
-        path: '/',
-        element: <LoginLayout />,
         children: [
           {
             path: 'login',
