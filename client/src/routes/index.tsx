9ed056810747c965e856c07d1d204038651f369c
Albert.Huang <albert.huang@qima.com>
2024-09-11 11:00:48 +0800

    librechat bak update,follow wiki.

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -54,6 +54,8 @@ export const router = createBrowserRouter([
     element: <AuthLayout />,
     children: [
       {
+        path: '/',
+        element: <LoginLayout />,
         children: [
           {
             path: 'login',
