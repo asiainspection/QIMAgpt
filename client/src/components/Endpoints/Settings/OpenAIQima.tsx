bd6cbe7879363d60c841c1bae47f0bea166eab72
You <anonymous@example.com>
2025-06-06 18:13:40 +0800

    Revert "chore: test setting"
    This reverts commit 4d15f4aa1e8052ccfa7f0bc146eefbc8f26c4693.

--- client/src/components/Endpoints/Settings/OpenAIQima.tsx
+++ client/src/components/Endpoints/Settings/OpenAIQima.tsx
@@ -1,65 +1,73 @@
-import { useMemo } from 'react';
-import { getSettingsKeys } from 'librechat-data-provider';
-import type { SettingDefinition, DynamicSettingProps } from 'librechat-data-provider';
+import TextareaAutosize from 'react-textarea-autosize';
 import type { TModelSelectProps } from '~/common';
-import { componentMapping } from '~/components/SidePanel/Parameters/components';
-import { presetSettings } from '~/components/SidePanel/Parameters/settings';
+import { SelectDropDown, Input, Label } from '~/components/ui';
+import { cn, defaultTextProps, removeFocusOutlines } from '~/utils/';
+import { useLocalize } from '~/hooks';
 
-export default function OpenAISettings({
-  conversation,
-  setOption,
-  models,
-  readonly,
-}: TModelSelectProps) {
-  const parameters = useMemo(() => {
-    const [combinedKey, endpointKey] = getSettingsKeys(
-      conversation?.endpointType ?? conversation?.endpoint ?? '',
-      conversation?.model ?? '',
-    );
-    return presetSettings[combinedKey] ?? presetSettings[endpointKey];
-  }, [conversation]);
-
-  if (!parameters) {
+export default function Settings({ conversation, setOption, models, readonly }: TModelSelectProps) {
+  const localize = useLocalize();
+  if (!conversation) {
     return null;
   }
+  const { model, chatGptLabel, promptPrefix } = conversation;
+  const endpoint = conversation.endpoint || 'openAI';
+  const isOpenAI = endpoint === 'openAI' || endpoint === 'azureOpenAI';
 
-  const renderComponent = (setting: SettingDefinition | undefined) => {
-    if (!setting) {
-      return null;
-    }
-    const Component = componentMapping[setting.component];
-    if (!Component) {
-      return null;
-    }
-    const { key, default: defaultValue, ...rest } = setting;
-
-    const props = {
-      key,
-      settingKey: key,
-      defaultValue,
-      ...rest,
-      readonly,
-      setOption,
-      conversation,
-    };
-
-    if (key === 'model') {
-      return <Component {...props} options={models} />;
-    }
-
-    return <Component {...props} />;
-  };
+  const setModel = setOption('model');
+  const setChatGptLabel = setOption('chatGptLabel');
+  const setPromptPrefix = setOption('promptPrefix');
 
   return (
-    <div className="h-auto max-w-full overflow-x-hidden p-3">
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-5">
-        <div className="flex flex-col gap-6 md:col-span-3">
-          {parameters.col1.map(renderComponent)}
-        </div>
-        <div className="flex flex-col gap-6 md:col-span-2">
-          {parameters.col2.map(renderComponent)}
-        </div>
+    <div className="col-span-5 flex flex-col items-center justify-start gap-6 sm:col-span-3">
+      <div className="grid w-full items-center gap-2">
+        <SelectDropDown
+          value={model ?? ''}
+          setValue={setModel}
+          availableValues={models}
+          disabled={readonly}
+          className={cn(defaultTextProps, 'flex w-full resize-none', removeFocusOutlines)}
+          containerClassName="flex w-full resize-none"
+        />
       </div>
+      {isOpenAI && (
+        <>
+          <div className="grid w-full items-center gap-2">
+            <Label htmlFor="chatGptLabel" className="text-left text-sm font-medium">
+              {localize('com_endpoint_custom_name')}{' '}
+              <small className="opacity-40">({localize('com_endpoint_default_blank')})</small>
+            </Label>
+            <Input
+              id="chatGptLabel"
+              disabled={readonly}
+              value={chatGptLabel || ''}
+              onChange={(e) => setChatGptLabel(e.target.value ?? null)}
+              placeholder={localize('com_endpoint_openai_custom_name_placeholder')}
+              className={cn(
+                defaultTextProps,
+                'flex h-10 max-h-10 w-full resize-none px-3 py-2',
+                removeFocusOutlines,
+              )}
+            />
+          </div>
+          <div className="grid w-full items-center gap-2">
+            <Label htmlFor="promptPrefix" className="text-left text-sm font-medium">
+              {localize('com_endpoint_prompt_prefix')}{' '}
+              <small className="opacity-40">({localize('com_endpoint_default_blank')})</small>
+            </Label>
+            <TextareaAutosize
+              id="promptPrefix"
+              disabled={readonly}
+              value={promptPrefix || ''}
+              onChange={(e) => setPromptPrefix(e.target.value ?? null)}
+              placeholder={localize('com_endpoint_openai_prompt_prefix_placeholder')}
+              className={cn(
+                defaultTextProps,
+                'flex max-h-[300px] min-h-[100px] w-full resize-none px-3 py-2 ',
+              )}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
