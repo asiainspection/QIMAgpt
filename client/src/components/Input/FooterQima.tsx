dd4f06e04896a54b3cb6c3e17c7948d15a3ab225
You <anonymous@example.com>
2025-06-06 15:45:51 +0800

    fix: refactor code structure and remove redundant changes

--- client/src/components/Input/FooterQima.tsx
+++ client/src/components/Input/FooterQima.tsx
@@ -1,7 +1,10 @@
-import { useGetStartupConfig } from 'librechat-data-provider/react-query';
+// import { useGetStartupConfig } from 'librechat-data-provider/react-query';
+import { useOutletContext, useSearchParams } from 'react-router-dom';
+import type { TLoginLayoutContext } from '~/common';
 
 export default function FooterQima() {
-  const { data: config } = useGetStartupConfig();
+  // const { data: config } = useGetStartupConfig();
+  const { config } = useOutletContext<TLoginLayoutContext>();
   return (
     <div className="hidden px-3 pb-1 pt-2 text-center text-xs text-black/50 dark:text-white/50 md:block md:px-4 md:pb-4 md:pt-3">
       <a href="" className="underline">
