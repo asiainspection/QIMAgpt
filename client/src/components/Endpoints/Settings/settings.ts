cc164eecc603147c299b2410bb83df498d7670cd
You <anonymous@example.com>
2025-08-17 22:06:12 +0800

    Revert "fix: build issue"
    This reverts commit be9c0dc2283244891e3610b50b2ef22b17d5356a.

--- client/src/components/Endpoints/Settings/settings.ts
+++ client/src/components/Endpoints/Settings/settings.ts
@@ -5,7 +5,7 @@ import { GoogleSettings, PluginSettings } from './MultiView';
 import AssistantsSettings from './Assistants';
 import AnthropicSettings from './Anthropic';
 import BedrockSettings from './Bedrock';
-import OpenAISettings from './OpenAI';
+import OpenAISettings from './OpenAIQima';
 
 const settings: { [key: string]: FC<TModelSelectProps> | undefined } = {
   [EModelEndpoint.assistants]: AssistantsSettings,
